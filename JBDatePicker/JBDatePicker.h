08d4b7be4e75a5f6edd20749444bc02b682fed3e
Josh Birnholz <josh@birnholz.com>
2019-10-30 11:35:01 -0400

    Initial Commit

---
+++ JBDatePicker/JBDatePicker.h
@@ -0,0 +1,19 @@
+//
+//  JBDatePicker.h
+//  JBDatePicker
+//
+//  Created by Josh Birnholz on 10/30/19.
+//  Copyright © 2019 Josh Birnholz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JBDatePicker.
+FOUNDATION_EXPORT double JBDatePickerVersionNumber;
+
+//! Project version string for JBDatePicker.
+FOUNDATION_EXPORT const unsigned char JBDatePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JBDatePicker/PublicHeader.h>
+
+
